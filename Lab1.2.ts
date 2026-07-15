89884ba9c6ff2b49c29c471f1a4701f1d4d2c06b
Sunhanat-021-png <mixxa4790@gmail.com>
2026-07-15 15:50:29 +0700

    ส่ง Lab1

---
+++ Lab1.2.ts
@@ -0,0 +1,8 @@
+let total: number = 0;
+
+for (let day = 1; day <= 30; day++) {
+    total += day
+    console.log(`วันที่ ${day}: ยอดเงิน = ${total} บาท`);
+}
+
+console.log(`ขอเเสดงความยินดีเดือนนี้คุณออมเงินได้ ${total} บาท`);
